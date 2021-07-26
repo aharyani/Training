bfc13a165abec13673a08073aa4ed7a1115c5aa8
NIS-PK371\aharyani <ammar_haryani@hotmail.com>
2021-07-26 15:11:58 +0500

    My Task3 initial

---
+++ Task3/src/PrimeNumber.java
@@ -0,0 +1,26 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        prime_number(50);
+    }
+
+    static void prime_number(int num) {
+        int flag;
+        int count = 0;
+        for (int i = 2; i < num; i++) {
+            flag = 1;
+            for (int j = 2; j < 10; j++) {
+
+                if (i % j == 0) {
+                    flag = 0;
+                    break;
+                }
+                System.out.println(j);
+            }
+            if (flag == 1) {
+                count++;
+            }
+        }
+        System.out.println("Total Prime Number till "+ (num-1) +" is "+ count);
+    }
+}

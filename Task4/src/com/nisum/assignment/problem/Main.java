1425ed4ab0d12dcc0be4724150c3797b2df94616
aharyani <ammar_haryani@hotmail.com>
2021-07-29 17:45:59 +0500

    Task4 ready for review

--- Task4/src/com/nisum/assignment/problem/Main.java
+++ Task4/src/com/nisum/assignment/problem/Main.java
@@ -3,6 +3,13 @@ package com.nisum.assignment.problem;
 public class Main {
     public static void main(String[] args) {
 
+        /**
+         * 1. Write a program in java that creates 3 Threads i.e Main, Thread1 and Thread2.
+         * Print execution count down messages for Thread 1 and Thread 2, also make Thread 1 and Thread 2 sleep for 1 second.
+         * Finally Main thread should end it's execution after Thread2 and Thread1 finish their execution
+         *
+         * Limitations: You can not use "sleep()" mehtod for "main" thread.
+         */
         try {
             Occurence.occur();
         } catch (MyCustomNullException exp) {

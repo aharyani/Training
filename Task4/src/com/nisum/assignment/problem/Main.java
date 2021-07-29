28f9f4184b3c7dd7d70545d6735b2d87564509dc
aharyani <ammar_haryani@hotmail.com>
2021-07-29 17:59:00 +0500

    Task 4 ready for review

--- Task4/src/com/nisum/assignment/problem/Main.java
+++ Task4/src/com/nisum/assignment/problem/Main.java
@@ -4,12 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         /**
-         * 1. Write a program in java that creates 3 Threads i.e Main, Thread1 and Thread2.
-         * Print execution count down messages for Thread 1 and Thread 2, also make Thread 1 and Thread 2 sleep for 1 second.
-         * Finally Main thread should end it's execution after Thread2 and Thread1 finish their execution
-         *
-         * Limitations: You can not use "sleep()" mehtod for "main" thread.
+         *1. Write a Java program to create a custom exception class that deals with NULL Strings 
+         *   (your custom exception should be thrown if String is NULL)
          */
+
         try {
             Occurence.occur();
         } catch (MyCustomNullException exp) {

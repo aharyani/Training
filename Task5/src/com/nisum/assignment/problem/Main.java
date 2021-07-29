f764a51b650775b9c7b380ab23d28b83f2ac922e
aharyani <ammar_haryani@hotmail.com>
2021-07-29 17:54:49 +0500

    Task5 ready for review

--- Task5/src/com/nisum/assignment/problem/Main.java
+++ Task5/src/com/nisum/assignment/problem/Main.java
@@ -2,7 +2,13 @@ package com.nisum.assignment.problem;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-
+    /**
+     * 1. Write a program in java that creates 3 Threads i.e Main, Thread1 and Thread2.
+     * Print execution count down messages for Thread 1 and Thread 2, also make Thread 1 and Thread 2 sleep for 1 second.
+     * Finally Main thread should end it's execution after Thread2 and Thread1 finish their execution
+     *
+     * Limitations: You can not use "sleep()" mehtod for "main" thread.
+     */
         Thread thread1 = new Thread(Counter::count,"Thread1");
         Thread thread2 = new Thread(Counter::count,"Thread2");
         thread1.start();

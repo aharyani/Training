a39a3436591b8d1f53764e3a9324de5f72b89e41
aharyani <ammar_haryani@hotmail.com>
2021-07-30 16:00:57 +0500

    Task1 modified

--- Task1/src/com/nisum/assignment/problem/two/point/two/Singleton.java
+++ Task1/src/com/nisum/assignment/problem/two/point/two/Singleton.java
@@ -3,22 +3,24 @@ package com.nisum.assignment.problem.two.point.two;
 import java.util.HashMap;
 
 public class Singleton {
-   public static HashMap<String,Singleton> instances = new HashMap<>();
+    public static HashMap<String, Singleton> instances = new HashMap<>();
     private static Singleton singleton;
-    private Singleton(){}
 
-    public static void getInstance(){
-        if(singleton==null){
-            synchronized (Singleton.class){
-                if(singleton==null){
+    private Singleton() {
+    }
+
+    public static void getInstance() {
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                if (singleton == null) {
                     singleton = new Singleton();
-                    instances.put(Thread.currentThread().getName(),singleton);
-                }else{
-                    instances.put(Thread.currentThread().getName(),singleton);
+                    instances.put(Thread.currentThread().getName(), singleton);
                 }
             }
-        }else{
-            instances.put(Thread.currentThread().getName(),singleton);
+        } else {
+            instances.put(Thread.currentThread().getName(), singleton);
         }
 
     }
+
+}

0676228577489e43abc10c05a10f4ed8165bf6f0
aharyani <ammar_haryani@hotmail.com>
2021-07-30 15:02:01 +0500

    Update Singleton.java

--- Task1/src/com/nisum/assignment/problem/two/point/two/Singleton.java
+++ Task1/src/com/nisum/assignment/problem/two/point/two/Singleton.java
@@ -22,14 +22,3 @@ public class Singleton {
         }
 
     }
-//    public static Singleton getInstance(){
-//        if(singleton==null){
-//            synchronized (Singleton.class){
-//                if(singleton==null){
-//                    singleton = new Singleton();
-//                }
-//            }
-//        }
-//        return singleton;
-//    }
-}
